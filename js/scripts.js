d434bd1551f5cd365cec9af8f318769a415d44c4
Jerrod Styrk <jstyrk@citadel.edu>
2020-11-02 11:27:26 -0800

    create object and print respective key-values to page
    Co-authored-by: Riley McAdoo <riley.mcadoo@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 // Business Logic
-function Place(city, country, landmarks, food, pop, lang) {
+function Place(city, country, landmark, food, pop, lang) {
   this.city = city;
   this.country = country;
-  this.landmarks = landmarks;
+  this.landmark = landmark;
   this.food = food;
   this.pop = pop;
   this.lang = lang;
@@ -10,13 +10,26 @@ function Place(city, country, landmarks, food, pop, lang) {
 
 
 
-
-
 // User Logic
 $(document).ready(function() {
-  $("").submit(function(event) {
+  $("#addPlace").submit(function(event) {
     event.preventDefault();
 
+    let city = $("input#addCity").val();
+    let country = $("input#addCountry").val();
+    let landmark = $("input#addLandmark").val();
+    let food = $("input#addFood").val();
+    let pop = $("input#addPop").val();
+    let lang = $("input#addLang").val();
+    let newPlace = new Place(city, country, landmark, food, pop, lang);
+
+    $("#showPlace").append(newPlace) 
+    $(".city").text(newPlace.city);
+    $(".country").text(newPlace.country);
+    $(".landmark").text(newPlace.landmark);
+    $(".food").text(newPlace.food);
+    $(".population").text(newPlace.pop);
+    $(".language").text(newPlace.lang);
   });
 });
 
