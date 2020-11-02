fd8008e08a0c2971cbb95b59f66bfd1d7e001720
Jerrod Styrk <jstyrk@citadel.edu>
2020-11-02 15:26:35 -0800

    add input places into a list on the application
    Co-authored-by: Riley McAdoo <riley.mcadoo@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,42 @@
 // Business Logic
+function PlaceList() {
+  this.places = [];
+  this.currentId = 0; 
+}
+
+PlaceList.prototype.addPlace = function(place) {
+  place.id = this.assignId();
+  this.places.push(place);
+}
+
+PlaceList.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+PlaceList.prototype.findPlace = function(id) {
+  for (let i=0; i< this.places.length; i++) {
+    if (this.places[i]) {
+      if (this.places[i].id == id) {
+        return this.places[i];
+      }
+    }
+  };
+  return false;
+}
+
+PlaceList.prototype.deletePlace = function(id) {
+  for (let i=0; i< this.places.length; i++) {
+    if(this.places[i]) {
+      if (this.places[i].id == id) {
+        delete this.places[i];
+        return true;
+      }
+    }
+  };
+  return false
+}
+
 function Place(city, country, landmark, food, pop, lang) {
   this.city = city;
   this.country = country;
@@ -8,9 +46,18 @@ function Place(city, country, landmark, food, pop, lang) {
   this.lang = lang;
 }
 
-
-
 // User Logic
+let placeList = new PlaceList();
+
+function displayPlaceDetails(placeListToDisplay) {
+  let destinationList = $("ul#places");
+  let htmlForPlacesInfo = "";
+  placeListToDisplay.places.forEach(function(place) {
+    htmlForPlacesInfo += "<li id=" + place.id + ">" + place.city + "</li>";
+  });
+  destinationList.html(htmlForPlacesInfo);
+};
+
 $(document).ready(function() {
   $("#addPlace").submit(function(event) {
     event.preventDefault();
@@ -22,8 +69,11 @@ $(document).ready(function() {
     let pop = $("input#addPop").val();
     let lang = $("input#addLang").val();
     let newPlace = new Place(city, country, landmark, food, pop, lang);
+    placeList.addPlace(newPlace);
+    displayPlaceDetails(placeList)
 
-    $("#showPlace").append(newPlace) 
+
+    $(".row").append(newPlace) 
     $(".city").text(newPlace.city);
     $(".country").text(newPlace.country);
     $(".landmark").text(newPlace.landmark);
@@ -31,5 +81,7 @@ $(document).ready(function() {
     $(".population").text(newPlace.pop);
     $(".language").text(newPlace.lang);
   });
+    $("h2").click(function() {
+      $("p").toggle();
+    });
 });
-
